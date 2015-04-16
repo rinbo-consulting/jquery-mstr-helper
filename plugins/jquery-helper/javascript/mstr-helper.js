287f978abbec093edf34518d5b50d4942d75941a
Paul Bailey <bailey@dreamshake.net>
2015-04-16 08:03:06 +0100

    Selector extension: first steps
    Signed-off-by: Paul Bailey <bailey@dreamshake.net>

--- plugins/jquery-helper/javascript/mstr-helper.js
+++ plugins/jquery-helper/javascript/mstr-helper.js
@@ -1,12 +1,35 @@
 (function($) {
-  $.getPanelByName = function(name) {
-    var elements = [];
-    for (key in mstrmojo.all) {
-      var obj = mstrmojo.all[key];
-      if ('scriptClass' in obj && obj.scriptClass === 'mstrmojo.ExpressDocPanel' && obj.title === strName) {
-        results.push(obj.domNode);
+  $.MicroStrategyObjectTypes = {
+    Panel: ['scriptClass', 'mstrmojo.ExpressDocPanel'],
+    DropDownList: ['cssClass', 'mstrmojo-DocSelector-DropDownList']
+  };
+  $.expr[':'].mstr = $.expr.createPseudo(function(arg) {
+    if (arg) {
+      var args = arg.split('=');
+      if (args.length == 1) {
+        var prop = 'title';
+        var propValue = arg;
+      } else if (args[0] == 'type' && $.MicroStrategyObjectTypes.hasOwnProperty(args[1])) {
+        var prop = $.MicroStrategyObjectTypes[args[1]][0];
+        var propValue = $.MicroStrategyObjectTypes[args[1]][1];
+      } else {
+        var prop = args[0];
+        var propValue = args[1];
+      }
+      return function(elem) {
+        var answer = false;
+        $.each(mstrmojo.all, function(key, value) {
+          answer = (value.domNode === elem) && (value[prop] === propValue);
+          return !answer;
+        });
+        return answer;
+      }
+
+    } else {
+      return function() {
+        return false;
       }
     }
-    return $(results);
-  }
+  });
+
 })(jQuery);
