c37f41855b59df79d75d10e54bf2163cbbc43ed5
Paul Bailey <bailey@dreamshake.net>
2015-04-16 12:17:58 +0100

    Teach the selector how to identify grids

--- plugins/jquery-helper/javascript/mstr-helper.js
+++ plugins/jquery-helper/javascript/mstr-helper.js
@@ -1,7 +1,8 @@
 (function($) {
   $.MicroStrategyObjectTypes = {
     Panel: ['scriptClass', 'mstrmojo.ExpressDocPanel'],
-    DropDownList: ['cssClass', 'mstrmojo-DocSelector-DropDownList']
+    DropDownList: ['cssClass', 'mstrmojo-DocSelector-DropDownList'],
+    Grid: ['gridData']
   };
   $.expr[':'].mstr = $.expr.createPseudo(function(arg) {
     if (arg) {
@@ -10,8 +11,13 @@
         var prop = 'title';
         var propValue = arg;
       } else if (args[0] == 'type' && $.MicroStrategyObjectTypes.hasOwnProperty(args[1])) {
-        var prop = $.MicroStrategyObjectTypes[args[1]][0];
-        var propValue = $.MicroStrategyObjectTypes[args[1]][1];
+        var identifiers = $.MicroStrategyObjectTypes[args[1]];
+        var prop = identifiers[0];
+        if (identifiers.length == 2) {
+          var propValue = identifiers[1];
+        } else {
+          var propValue = undefined;
+        }
       } else {
         var prop = args[0];
         var propValue = args[1];
@@ -19,17 +25,19 @@
       return function(elem) {
         var answer = false;
         $.each(mstrmojo.all, function(key, value) {
-          answer = (value.domNode === elem) && (value[prop] === propValue);
+          if (propValue) {
+            answer = (value.domNode === elem) && (value[prop] === propValue);
+          } else {
+            answer = (value.domNode === elem) && value.hasOwnProperty(prop);
+          }
           return !answer;
         });
         return answer;
       }
-
     } else {
       return function() {
         return false;
       }
     }
   });
-
 })(jQuery);
