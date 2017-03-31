a638465cc3f1c53056cac35a5453ccf7e42f2b54
Paul Bailey <paul.bailey@rinbo.consulting>
2017-03-31 17:12:29 +0100

    Load libraries conditionally

--- plugins/jquery-helper/javascript/rwdPage.js
+++ plugins/jquery-helper/javascript/rwdPage.js
@@ -1,9 +1,18 @@
 
 var setupJQuery = function(callback) {
-    if (mstrmojo) {
-        mstrmojo._LoadsScript.requiresExternalScripts(
-            [{url: '//code.jquery.com/jquery-1.11.2.min.js'},
-            {url: '../plugins/jquery-helper/javascript/mstr-helper.js'}],
+    var scriptsToLoad = [];
+    if ($().jquery) {
+      console.log("setupJQuery function says: jQuery is already loaded.");
+    } else {
+      scriptsToLoad.push({url: '//code.jquery.com/jquery-1.11.2.min.js'});
+    }
+    if ($.MicroStrategyObjectTypes) {
+      console.log("setupJQuery function says: jquery-mstr-helper is already loaded.");
+    } else {
+      scriptsToLoad.push({url: '../plugins/jquery-helper/javascript/mstr-helper.js'});
+    }
+    if (mstrmojo && scriptsToLoad.length > 0) {
+        mstrmojo._LoadsScript.requiresExternalScripts(scriptsToLoad,
             callback);
     }
 };
